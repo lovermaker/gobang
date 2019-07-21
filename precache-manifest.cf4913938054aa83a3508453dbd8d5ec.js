0a432140dcc5fcc85f7d433e535778d9a40cf475
LoverMaker <867606315@qq.com>
2019-07-21 15:47:33 +0800

    Updates

---
+++ precache-manifest.cf4913938054aa83a3508453dbd8d5ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9899e38aa7a78b4234136139772880ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e25f1d436fdb0a1f4b3f",
+    "url": "./static/css/2.a456a614.chunk.css"
+  },
+  {
+    "revision": "f555881949a6ee74f621",
+    "url": "./static/css/main.48778557.chunk.css"
+  },
+  {
+    "revision": "e25f1d436fdb0a1f4b3f",
+    "url": "./static/js/2.a3d8e3e7.chunk.js"
+  },
+  {
+    "revision": "f555881949a6ee74f621",
+    "url": "./static/js/main.b56263d4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5442f90f0a7d84ccc30d008200e3aee6",
+    "url": "./static/media/board.5442f90f.jpg"
+  },
+  {
+    "revision": "6a136b2f85e48a7228c945b19c0f4097",
+    "url": "./static/media/person.6a136b2f.jpeg"
+  }
+]);
